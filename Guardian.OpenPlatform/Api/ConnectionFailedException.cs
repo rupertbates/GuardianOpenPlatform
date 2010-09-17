887ef604f9fc7b6f96b54b55d8ee47fe05988487
rupertbates <rupert.bates@guardian.co.uk>
2010-09-17 19:28:35 +0100

    Moving stuff around

---
+++ Guardian.OpenPlatform/Api/ConnectionFailedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guardian.OpenPlatform
+{
+    public class ConnectionFailedException : Exception
+    {
+        public ConnectionFailedException(string query)
+        {
+            Query = query;
+        }
+        public string Query { get; private set; }
+    }
+}
---
+++ Guardian.OpenPlatform/Results/ObjectDeserializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using Guardian.OpenPlatform.Results.Entities;
+using Guardian.OpenPlatform.Results.SearchResponses;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Guardian.OpenPlatform.Results;
+
+namespace Guardian.OpenPlatform
+{
+    public class ObjectDeserializer
+    {
+        public T Deserialize<T>(string json) where T : ResponseBase
+        {
+            var wrapper = JsonConvert.DeserializeObject<ResponseWrapper<T>>(json);
+            return (T) wrapper.Response;
+        }
+    }
+}
