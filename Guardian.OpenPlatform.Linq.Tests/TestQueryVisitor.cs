d21ace7f972d9077ec5b32f00f9a100d114dbc53
rupertbates <rupert.bates@guardian.co.uk>
2010-09-19 20:45:11 +0100

    Adding a new expressionVisitor and tests

---
+++ Guardian.OpenPlatform.Linq.Tests/TestQueryVisitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Guardian.OpenPlatform.Results.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Guardian.OpenPlatform.Linq.Generic;
+
+namespace Guardian.OpenPlatform.Linq.Tests
+{
+    [TestClass]
+    public class TestQueryVisitor
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var vis = new ContentQueryVisitor();
+            var context = new OpenPlatformQueryContext();
+            //Expression ex = 
+            //vis.Translate();
+        }
+    }
+}
---
+++ Guardian.OpenPlatform.Linq/Generic/ContentQueryVisitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Guardian.OpenPlatform.Linq.Generic
+{
+    public class ContentQueryVisitor : ExpressionVisitor
+    {
+        private ContentSearchParameters _searchParameter = new ContentSearchParameters();
+        public ContentSearchParameters Translate(Expression ex)
+        {
+            Visit(ex);
+            return _searchParameter;
+        }
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.Name == "Where")
+                //innermostWhereExpression = expression;
+
+            Visit(node.Arguments[0]);
+
+            return node;
+        }
+    }
+}
